d272990d11264b49440fc184af38f0662d8968a3
hadi fauzan <mfauzan355@gmail.com>
2024-09-06 09:31:47 +0700

    js 2 prak 1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function (){
+    return ('Hello World');
+});
+
+Route::get('/world', function (){
+    return (' World');
+});
+
+
+Route::get('/', function (){
+    return ('Selamat datang');
+});
+
+
+Route::get('/about', function (){
+    return ('NIM : 2241760039 & Nama : Muhammad Khasbul Hadi Fauzan');
+});
+
+Route::get ('/user/{name} ', function ($name){
+    return 'Nama saya '. $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'. $postId." Komentar ke -: ".$commentId;
+});
+
+
+Route::get('/articles/{id}', function ($id){
+    return ('Halaman Artikel dengan ID '.$id);
+});
+
+Route::get ('/user/{name?}', function ($name='john') {
+    return 'Nama saya '. $name;
 });
